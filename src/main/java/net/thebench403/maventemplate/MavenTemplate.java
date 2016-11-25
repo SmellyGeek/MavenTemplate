6a6cb75efcd584a0391722940a407b7360003c83
Corin22225 <mrcoltj@gmail.com>
2016-11-25 13:52:45 -0500

    Initial commit

---
+++ src/main/java/net/thebench403/maventemplate/MavenTemplate.java
@@ -0,0 +1,16 @@
+package net.thebench403.maventemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MavenTemplate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Insert what you want to do on plugin enable here
+    }
+
+    @Override
+    public void onDisable() {
+        // Insert what you want to do on plugin disable here
+    }
+}
